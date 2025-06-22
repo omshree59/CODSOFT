5bdc71465d7291733ff5d22fc05dfc33067263af
omshree59 <paridaomshree@gmail.com>
2025-06-22 20:26:47 +0530

    Add files via upload

---
+++ TASK2/script.js
@@ -0,0 +1,11 @@
+const elements = document.querySelectorAll('.fade-in, .fade-up');
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('appear');
+    }
+  });
+}, { threshold: 0.1 });
+
+elements.forEach(el => observer.observe(el));
